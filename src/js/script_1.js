1d66951018df442ae172ea13a68018a1d14d7029
tony <zhova24@gmail.com>
2021-01-15 05:07:23 +0200

    added reviews screen

--- src/js/script_1.js
+++ src/js/script_1.js
@@ -31,6 +31,14 @@ $(document).ready(function () {
     prevArrow:
       '<div class="btn_default btn_big default-slider__arrow default-slider__arrow-left">Назад</div>',
   });
+  $(".reviews-slider").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow:
+      '<div class="btn_default btn_big default-slider__arrow default-slider__arrow-right">Дальше</div>',
+    prevArrow:
+      '<div class="btn_default btn_big default-slider__arrow default-slider__arrow-left">Назад</div>',
+  });
   $(".slick-dots li button").text("");
   $(".slick-dots li button").wrap("<div class='dots-button-wrap'></div>");
 });
