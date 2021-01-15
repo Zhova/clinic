6c76e86ea0bd18a9c8741808f36a95f8aedddeb5
tony <zhova24@gmail.com>
2021-01-15 07:50:24 +0200

    added responsive home page

--- src/js/script_1.js
+++ src/js/script_1.js
@@ -1,43 +1,60 @@
 $(document).ready(function () {
   $(".main-slider").slick({
-    // slidesToShow: 1,
-    // slidesToScroll: 1,
     nextArrow:
       '<div class="main-slider__arrow main-slider__arrow-right"></div>',
     prevArrow: '<div class="main-slider__arrow main-slider__arrow-left"></div>',
     dots: true,
   });
-  $(".popularDest-slider").slick({
+  $(".popularDest-slider, .specialists-slider").slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     nextArrow:
       '<div class="btn_default btn_big default-slider__arrow default-slider__arrow-right">Дальше</div>',
     prevArrow:
       '<div class="btn_default btn_big default-slider__arrow default-slider__arrow-left">Назад</div>',
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 590,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
-  $(".specialists-slider").slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    nextArrow:
-      '<div class="btn_default btn_big default-slider__arrow default-slider__arrow-right">Дальше</div>',
-    prevArrow:
-      '<div class="btn_default btn_big default-slider__arrow default-slider__arrow-left">Назад</div>',
-  });
-  $(".news-slider").slick({
+
+  $(".reviews-slider, .news-slider").slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     nextArrow:
       '<div class="btn_default btn_big default-slider__arrow default-slider__arrow-right">Дальше</div>',
     prevArrow:
       '<div class="btn_default btn_big default-slider__arrow default-slider__arrow-left">Назад</div>',
-  });
-  $(".reviews-slider").slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    nextArrow:
-      '<div class="btn_default btn_big default-slider__arrow default-slider__arrow-right">Дальше</div>',
-    prevArrow:
-      '<div class="btn_default btn_big default-slider__arrow default-slider__arrow-left">Назад</div>',
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 790,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
   $(".slick-dots li button").text("");
   $(".slick-dots li button").wrap("<div class='dots-button-wrap'></div>");
